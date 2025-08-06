11dbcf975d95408f4c78b11deb5ed05cc70ccda0
JUAN ANTONIO CASTILLA ORTA <L21430327@piedrasnegras.tecnm.mx>
2025-08-05 22:32:49 -0500

    Referencia alumno a user

--- database/migrations/2025_08_06_013152_create_alumnos_table.php
+++ database/migrations/2025_08_06_013152_create_alumnos_table.php
@@ -13,12 +13,15 @@ return new class extends Migration
     {
         Schema::create('alumnos', function (Blueprint $table) {
              $table->string('id',15)->primary();
+            $table->unsignedBigInteger('user_id')->nullable(); // Relación con users
             $table->string('nombreAlumno',50);
             $table->string('apellidoPaterno',50);
             $table->string('apellidoMaterno',50)->nullable();
             $table->string('sexo',1);
             $table->string('idCarrera',15);
             $table->foreign('idCarrera')->references('id')->on('carreras');
+            
+    $table->foreign('user_id')->references('id')->on('users')->onDelete('set null');
             $table->timestamps();
         });
     }
